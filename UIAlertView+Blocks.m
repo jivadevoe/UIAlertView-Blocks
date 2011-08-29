8cf472a78b187af3be744062ee059a8d216cf239
Vytis Sibonis <vytis@proto.lt>
2011-08-29 17:01:22 +0300

    Fixed a crash when more than one alert view is displayed at a time

--- UIAlertView+Blocks.m
+++ UIAlertView+Blocks.m
@@ -57,7 +57,7 @@ static NSString *RI_BUTTON_ASS_KEY = @"com.random-ideas.BUTTONS";
 	
 }
 
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
     NSArray *buttonsArray = objc_getAssociatedObject(self, RI_BUTTON_ASS_KEY);
     RIButtonItem *item = [buttonsArray objectAtIndex:buttonIndex];
